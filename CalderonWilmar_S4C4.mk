85bcd6a04bb82c89b01efd96dafa755b74ae9fca
wilmarc <wa.calderon@uniandes.edu.co>
2018-07-03 11:56:10 -0500

    Arreglos al .mk

--- CalderonWilmar_S4C4.mk
+++ CalderonWilmar_S4C4.mk
@@ -3,11 +3,16 @@
 resultados.pdf : resultados.tex plot1.pdf plot.pdf
 	pdflatex resultados.tex
 
-plot.pdf : makedatos.py
-	python makedatos.py > datos.dat
+plot.pdf : datos.dat
 	python plotdatos.py	
 
-plot1.pdf : makedatos1.py
-	python makedatos1.py > datos1.dat
+plot1.pdf : datos1.dat
 	python plotdatos1.py
+
+datos.dat : makedatos.py
+	python makedatos.py > datos.dat
+
+datos1.dat : makedatos1.py
+	python makedatos1.py > datos1.dat
+
 
